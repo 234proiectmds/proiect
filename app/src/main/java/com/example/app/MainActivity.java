a5a77e01c85b831dbc8dc997f56a246569c10be1
StefaniaCri <79380914+StefaniaCri@users.noreply.github.com>
2022-05-08 23:22:45 +0300

    MainActivityPage

---
+++ app/src/main/java/com/example/app/MainActivity.java
@@ -0,0 +1,43 @@
+package com.example.app;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.animation.Animator;
+import android.animation.AnimatorListenerAdapter;
+import android.content.Intent;
+import android.os.Bundle;
+import android.os.Handler;
+import android.widget.ImageView;
+import android.widget.TextView;
+
+public class MainActivity extends AppCompatActivity {
+
+    ImageView imageView;
+    TextView textView;
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+
+        imageView = findViewById(R.id.imageView);
+        textView = findViewById(R.id.TextView);
+
+        imageView.animate().alpha(0f).setDuration(0);
+        textView.animate().alpha(0f).setDuration(0);
+
+        imageView.animate().alpha(1f).setDuration(1000).setListener(new AnimatorListenerAdapter() {
+            @Override
+            public void onAnimationEnd(Animator animation) {
+                textView.animate().alpha(1f).setDuration(1000);
+            }
+        });
+        new Handler().postDelayed(new Runnable() {
+            @Override
+            public void run() {
+                Intent intent = new Intent(MainActivity.this, MainMenu.class);
+                startActivity(intent);
+                finish();
+            }
+        },3000);
+    }
+}

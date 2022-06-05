21833a1f0668446059adf6f51a6a67a74a790601
AlexandraBoghiu <aboghiu2001@gmail.com>
2022-06-05 12:09:04 +0300

    Adauga pagina client

--- app/src/main/java/com/example/app/ClientCakes_Navigation.java
+++ app/src/main/java/com/example/app/ClientCakes_Navigation.java
@@ -1,14 +1,58 @@
 package com.example.app;
 
+import androidx.annotation.NonNull;
 import androidx.appcompat.app.AppCompatActivity;
+import androidx.fragment.app.Fragment;
 
 import android.os.Bundle;
+import android.view.MenuItem;
 
-public class ClientCakes_Navigation extends AppCompatActivity {
+import com.example.app.clientCakesPanel.ClientCartFragment;
+import com.example.app.clientCakesPanel.ClientHomeFragment;
+import com.example.app.clientCakesPanel.ClientOrderFragment;
+import com.example.app.clientCakesPanel.ClientProfileFragment;
+import com.example.app.clientCakesPanel.ClientTrackFragment;
+import com.google.android.material.bottomnavigation.BottomNavigationView;
+
+public class ClientCakes_Navigation extends AppCompatActivity implements BottomNavigationView.OnNavigationItemSelectedListener {
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_client_cakes_navigation);
+        BottomNavigationView navigationView = findViewById(R.id.client_bottom_navigation);
+        navigationView.setOnNavigationItemSelectedListener(this);
+    }
+
+    @Override
+    public boolean onNavigationItemSelected(@NonNull MenuItem item) {
+        Fragment fragment = null;
+        switch (item.getItemId()){
+            case R.id.clientHome:
+                fragment = new ClientHomeFragment();
+                break;
+            case R.id.clientCart:
+                fragment = new ClientCartFragment();
+                break;
+            case R.id.clientOrders:
+                fragment = new ClientOrderFragment();
+                break;
+            case R.id.clientTrack:
+                fragment = new ClientTrackFragment();
+                break;
+            case R.id.clientProfile:
+                fragment = new ClientProfileFragment();
+                break;
+        }
+        return loadClientFragment(fragment);
+    }
+    private boolean loadClientFragment(Fragment fragment)
+    {
+        if (fragment != null)
+        {
+            getSupportFragmentManager().beginTransaction().replace(R.id.frame_container, fragment).commit();
+            return true;
+        }
+        return false;
     }
 }
---
+++ app/src/main/java/com/example/app/clientCakesPanel/ClientOrderFragment.java
@@ -0,0 +1,22 @@
+package com.example.app.clientCakesPanel;
+
+import android.os.Bundle;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+
+import androidx.annotation.NonNull;
+import androidx.annotation.Nullable;
+import androidx.fragment.app.Fragment;
+
+import com.example.app.R;
+
+public class ClientOrderFragment extends Fragment {
+    @Nullable
+    @Override
+    public View onCreateView(@NonNull LayoutInflater inflater, @Nullable ViewGroup container, @Nullable Bundle savedInstanceState) {
+        View v = inflater.inflate(R.layout.fragment_client_orders, null);
+        getActivity().setTitle("Comenzi");
+        return v;
+    }
+}

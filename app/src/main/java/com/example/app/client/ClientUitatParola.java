124fbb006ab4510dd7e5cc2fb3fc34f39eafbe9d
StefaniaCri <79380914+StefaniaCri@users.noreply.github.com>
2022-06-14 16:18:37 +0300

    Uitat parola  client (code)

--- app/src/main/java/com/example/app/client/ClientUitatParola.java
+++ app/src/main/java/com/example/app/client/ClientUitatParola.java
@@ -1,15 +1,74 @@
 package com.example.app.client;
 
+import androidx.annotation.NonNull;
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.content.Intent;
 import android.os.Bundle;
+import android.text.TextUtils;
+import android.view.View;
+import android.widget.Button;
+import android.widget.EditText;
+import android.widget.ProgressBar;
+import android.widget.Toast;
 
 import com.example.app.R;
+import com.example.app.admin.AdminLoginEmail;
+import com.example.app.admin.AdminUitatParola;
+import com.google.android.gms.tasks.OnCompleteListener;
+import com.google.android.gms.tasks.Task;
+import com.google.firebase.auth.FirebaseAuth;
 
 public class ClientUitatParola extends AppCompatActivity {
+
+
+    private EditText inputEmail;
+    private Button btnReset, btnInapoi;
+    private FirebaseAuth Fauth;
+    private ProgressBar progressBar;
+
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_client_uitat_parola);
+        inputEmail = (EditText) findViewById(R.id.inputEmail);
+        btnReset = (Button) findViewById(R.id.btnReset);
+        btnInapoi = (Button) findViewById(R.id.btnBack);
+        progressBar = (ProgressBar) findViewById(R.id.progressBar);
+        Fauth = FirebaseAuth.getInstance();
+
+        btnInapoi.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                startActivity(new Intent(ClientUitatParola.this, ClientLoginEmail.class));
+                finish();
+            }
+        });
+
+        btnReset.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                String email = inputEmail.getText().toString().trim();
+                if (TextUtils.isEmpty(email)) {
+                    Toast.makeText(getApplication(), "Introdu emailul cu care te-ai logat", Toast.LENGTH_SHORT).show();
+                    return;
+                }
+                progressBar.setVisibility(View.VISIBLE);
+
+                Fauth.sendPasswordResetEmail(email)
+
+                        .addOnCompleteListener(new OnCompleteListener<Void>() {
+                            @Override
+                            public void onComplete(@NonNull Task<Void> task) {
+                                if (task.isSuccessful()) {
+                                    Toast.makeText(ClientUitatParola.this, "Am trimis link-ul de resetare a parolei", Toast.LENGTH_SHORT).show();
+                                } else {
+                                    Toast.makeText(ClientUitatParola.this, "Eroare la trimiterea emailului!", Toast.LENGTH_SHORT).show();
+                                }
+                                progressBar.setVisibility(View.GONE);
+                            }
+                        });
+            }
+        });
     }
 }
